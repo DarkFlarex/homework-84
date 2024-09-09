5572375f8cde8675c2e835a1d7abddfa3d2350e5
DarkFlarex <darkflarexxx@gmail.com>
2024-09-10 01:54:43 +0600

    Добавлен пост запрос в роут tasks для создание задачи и проверки на залогиненность пользователя через token.

---
+++ todoList-api/routers/tasks.ts
@@ -0,0 +1,33 @@
+import auth, {RequestWithUser} from "../middleware/auth";
+import express from "express";
+import {TaskMutation} from "../types";
+import Task from "../models/Task";
+import mongoose from "mongoose";
+
+const tasksRouter = express.Router();
+
+tasksRouter.post('/', auth, async (req: RequestWithUser, res, next) => {
+    try{
+        if (!req.user) {
+            return res.status(401).send({ error: 'User not found' });
+        }
+        const taskMutation: TaskMutation = {
+            user: req.body.user,
+            title: req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+        };
+        const task = new Task(taskMutation);
+        await task.save();
+
+        return res.send(task);
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send(error);
+        }
+
+        return next(error);
+    }
+});
+
+export default tasksRouter;
