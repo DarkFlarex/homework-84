b433704254fbf375c57423c49537bf59ab2e0596
DarkFlarex <darkflarexxx@gmail.com>
2024-09-09 20:01:45 +0600

    Добавил директорию middleware для аутентификации пользователя по токену. Добавили типы данных в types

--- todoList-api/config.ts
+++ todoList-api/config.ts
@@ -19,7 +19,7 @@ const config = {
   rootPath,
   publicPath: path.join(rootPath, 'public'),
   corsOptions,
-  database: 'mongodb://localhost/shop',
+  database: 'mongodb://localhost/todoList',
 };
 
 export default config;
